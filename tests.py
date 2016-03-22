446da2ceffb49fe694026c3e8d3c7f24cdcc4215
Davy Peter Braun <hello@davybraun.com>
2016-03-22 18:08:43 +0100

    Create test for console output.

--- tests.py
+++ tests.py
@@ -1,15 +1,41 @@
 """Test suite for Mann."""
 # -*- coding: utf-8 -*-
+import sys
 import unittest
+from io import StringIO
 from colour_runner import runner as crunner
-# from mypleasure.mann import Mann
+from mypleasure.mann import Mann
 
 
 class ConsoleTestCase(unittest.TestCase):
     """Test console logger."""
 
     def runTest(self): # noqa
-        pass
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            logger = Mann(console=True)
+            logger.log('foo')
+
+            output = out.getvalue().strip()
+
+            self.assertEqual(output, 'foo')
+        finally:
+            sys.stdout = sys.__stdout__
+
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            logger = Mann()
+            logger.log('foo')
+
+            output = out.getvalue().strip()
+
+            self.assertEqual(output, '')
+        finally:
+            sys.stdout = sys.__stdout__
 
 
 def suite():
