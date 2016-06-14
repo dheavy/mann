52a15c54806a2ad81a36f7242df6991d7348ce35
Davy Peter Braun <hello@davybraun.com>
2016-06-14 13:12:43 +0200

    Add setup.py for distrib.

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup for Mann on PyPi."""
+from distutils.core import setup
+
+setup(
+    name='mann',
+    packages=['mann'],
+    version='0.9.1',
+    description='A multi-purpose logger and notifier.',
+    author='Davy Peter Braun',
+    author_email='davy@mypleasu.re',
+    url='https://github.com/mypleasureteam/mann',
+    download_url='https://github.com/mypleasureteam/mann/tarball/0.1',
+    keywords=['logging', 'notification', 'trello', 'slack'],
+    classifiers=[],
+)
